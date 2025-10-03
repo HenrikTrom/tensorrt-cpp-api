0b890bfa9cfb9ee46da34d909f4788aaf363979e
Henrik <training.deepl@gmail.com>
2025-10-03 12:21:17 +0200

    UPDATE: Fixed broken installation

--- src/cmd_line_parser.h
+++ src/cmd_line_parser.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "engine.h"
+#include "tensorrt-cpp-api/engine.h"
 #include <iostream>
 
 struct CommandLineArguments {
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "cmd_line_parser.h"
-#include "logger.h"
-#include "engine.h"
+#include "tensorrt-cpp-api/logger.h"
+#include "tensorrt-cpp-api/engine.h"
 #include <chrono>
 #include <opencv2/cudaimgproc.hpp>
 #include <opencv2/opencv.hpp>
